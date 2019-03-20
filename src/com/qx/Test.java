e725331c6aec09358e327cb111407258a223e764
wkl <1639434393@qq.com>
2019-03-20 16:36:09 +0800

    wkl 提交

---
+++ src/com/qx/Test.java
@@ -0,0 +1,12 @@
+package com.qx;
+
+/**
+ * Created by Dangerous on 2019/3/20.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helllo,word!");
+
+        System.out.println("比悲伤更悲伤的故事");
+    }
+}
